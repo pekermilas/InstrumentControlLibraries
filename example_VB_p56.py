68a125ae9696f8c48cde68fd67375d2c688a7bd6
pekermilas <46932127+pekermilas@users.noreply.github.com>
2025-10-15 10:00:22 -0400

    Figured pulling the counter buffer
    Instead of querying QA, I figured I need to query FA. This is tested in the code this time.

--- example_VB_p56.py
+++ example_VB_p56.py
@@ -17,21 +17,34 @@ import sr400_PhotonCounter_Control as sr400
 
 pcounter = sr400.sr400()
 pcounter.open()
-pcounter.mode_counterToInput(counter = 'A', counterInput = '10MHz')
-# pcounter.mode_counterToInput(counter = 'A', counterInput = 'input1')
+# pcounter.interface_fullReset()
+# pcounter.mode_counterToInput(counter = 'A', counterInput = '10MHz')
+pcounter.mode_counterToInput(counter = 'A', counterInput = 'input1')
 pcounter.frontPanel_counterReset()
-pcounter.mode_scanPeriods(num = 1)
-pcounter.frontPanel_counterStart()
+# pcounter.mode_scanPeriods(num = 4)
+# pcounter.frontPanel_counterStart()
 
 # for i in range(50):
 #     # print(pcounter.interface_readStatusByte(bit = 1))
 #     pcounter.interface_readStatusByte(bit = 1)
 #     time.sleep(0.2)
 
-while(not bool(int(pcounter.interface_readStatusByte(bit = 1)))):
-    pass
+# for i in range(4):
+#     # while(not bool(int(pcounter.interface_readStatusByte(bit = 1)))):
+#     while(int(pcounter.data_readCounterFinished('A', scanPoint=i+1))<0):
+#         pass
+#     print(pcounter.data_readCounterFinished(counter = 'A' , scanPoint=i+1))
 
-print(pcounter.data_readCounterFinished(counter = 'A'))
+# pcounter.data_readCounterFinished(counter = 'A', scanPoint = 4)
+
+for i in range(100):
+    print(pcounter.query('FA'))
+
+
+# while(not bool(int(pcounter.interface_readStatusByte(bit = 1)))):
+#     pass
+
+# print(pcounter.data_readCounterFinished(counter = 'A'))
     
 # for i in range(1):
 #     while(not bool(int(pcounter.interface_readStatusByte(bit = 1)))):
