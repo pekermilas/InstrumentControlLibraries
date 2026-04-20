1e9524c897758dff69ed75c02ee872c9750703b3
pekermilas <46932127+pekermilas@users.noreply.github.com>
2026-04-20 16:50:03 -0400

    Update Impedance Analyzer code
    This is the new version of standalone control code for Impedance Analyzer. Current constants are implemented as user input and contained in a dictionary. In case of additional parameters a dictionary will be populated. Also notes for the user are added to the top of the code as comments.

--- zurichInstruments_Control.py
+++ zurichInstruments_Control.py
@@ -4,6 +4,29 @@ Created on Fri Apr 17 16:06:33 2026
 
 @author: pekermilas
 """
+# EXAMPLE CALLS
+# import zurichInstruments_Control as ziC
+# t = ziC.ziDevice()
+# t.connectDevice()
+# t.setConstants()
+# t.close()
+
+# # Below is Zhinst Toolkit convention!!!!
+# devs = list(t.device)
+# pars = [0] * len(devs)
+# for i in range(len(devs)):
+#     pars[i] = devs[i][1]['Node'].split('/')[1:]
+
+# pars = pd.DataFrame(pars)
+# tabs = list(set(pars.iloc[:,1]))
+
+# a = [list(device.tu)[i][1]['Options'] for i in range(len(list(device.tu))) 
+#  if str(list(device.tu)[i][0])=='/dev32271/tu/thresholds/0/input']
+
+# For getting the parameter values one can check the 
+# Node values for a parameter as below
+# # a = list(device.tu)
+# # b = list(device.triggers)
 
 import time
 import zhinst.core
@@ -13,9 +36,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-discovery = zi.ziDiscovery()
-device_id = discovery.find('dev32271')
-device_props = discovery.get(device_id)
 
 class ziDevice:
 
@@ -40,36 +60,100 @@ class ziDevice:
         self.device = self.session.connect_device(self.devSerial)
         
     def getConstants(self):
-        self.consts['Oscillation Frequency'] = input("Please enter Oscillation Frequency in Hz: ")
-        self.consts['Max bandwidth'] = input("Please enter Maximum Bandwidth in Hz: ")
-        
-    
-    
-    def setConstants(self):
-        
-        self.session.daq_server.set('/dev32271/imps/0/freq', 501000)
-        self.session.daq_server.set('/dev32271/imps/0/maxbandwidth', 10000)
-        self.session.daq_server.set('/dev32271/imps/0/auto/inputrange', ???)
-        self.session.daq_server.set('/dev32271/imps/0/current/range', ???)
-        self.session.daq_server.set('/dev32271/imps/0/voltage/range', ???)
-        self.session.daq_server.set('/dev32271/imps/0/omegasuppression', ???)
-        self.session.daq_server.set('/dev32271/imps/0/demod/rate', ???)
-        self.session.daq_server.set('/dev32271/imps/0/mode', 1)
-        
-        self.session.daq_server.set('/dev32271/tu/thresholds/0/input', 59)
-        self.session.daq_server.set('/dev32271/tu/thresholds/0/activationtime', 0.006)
-        self.session.daq_server.set('/dev32271/tu/thresholds/0/deactivationtime', 0.003)
-        self.session.daq_server.set('/dev32271/tu/logicunits/0/inputs/0/not', 1)
-        
-        self.session.daq_server.set('/dev32271/auxouts/0/outputselect', 13)
-        self.session.daq_server.set('/dev32271/auxouts/0/scale', -3)
-        self.session.daq_server.set('/dev32271/auxouts/0/offset', -2)
-        self.session.daq_server.set('/dev32271/auxouts/0/limitlower', -2)
-        self.session.daq_server.set('/dev32271/auxouts/0/limitupper', 0)
-        
-        self.session.daq_server.set('/dev32271/sigouts/0/add', 1)
+        self.consts['Oscillation Frequency'] = \
+            input("Please enter Oscillation Frequency (Hz): ") or 501000
+        self.consts['Max bandwidth'] = \
+            input("Please enter Maximum Bandwidth (Hz): ") or 10000
+        self.consts['Input Control'] = \
+            input("Please enter Input Control (0:Manual, 1:Auto, 2:Current Zone): ") or 0
+        self.consts['Current Range'] = \
+            input("Please enter Input Current Range (A): ") or 0.010
+        self.consts['Voltage Range'] = \
+            input("Please enter Input Voltage Range (V): ") or 3
+        self.consts['Omega Suppression'] = \
+            input("Please enter Omega Suppression (dB): ") or 80
+        self.consts['Data Transfer Rate'] = \
+            input("Please enter Data Transfer Rate (Sa/s): ") or 60000
+        self.consts['Equivalent Circuit Mode'] = \
+            input("Please enter Equivalent Circuit Mode (0: 4-Terminal, 1: 2-Terminal): ") or 0
 
-        self.session.daq_server.set('/dev32271/triggers/out/0/source', 1)
+        self.consts['Threshold Input Signal'] = \
+            input("Please enter Threshold Input Signal (59: TU Output Value, \
+                  58: Aux Output Overload, 56: Aux Input Overload, \
+                  55: Output Overload, 54: Input(I) Overload, 53: Input(V) Overload, \
+                  52: Trigger Out, 51: Trigger In, 50: DIO, 3: Demod Theta, \
+                  2: Demod R, 1: Demod Y, 0: Demod X): ") or 59
+        self.consts['State Enable Time'] = \
+            input("Please enter State Enable Time (s): ") or 0.006
+        self.consts['State Disable Time'] = \
+            input("Please enter State Disable Time (s): ") or 0.003
+        self.consts['Logic Unit Not'] = \
+            input("Please enter Logic Unit Not (0: Off, 1: On ): ") or 1
+
+        self.consts['Aux Output Signal'] = \
+            input("Please enter Aux Output Signal (0: Demod X, 1: Demod Y, \
+                  2: Demod R, 3: Demod Theta, 11: TU Filtered Value, \
+                  12: Manual, 13: TU Output Value): ") or 13
+        self.consts['Aux Output Scale'] = \
+            input("Please enter Aux Output Scale (V): ") or -2
+        self.consts['Aux Output Offset'] = \
+            input("Please enter Aux Output Offset (V): ") or -2
+        self.consts['Aux Output Lower Limit'] = \
+            input("Please enter Aux Output Lower Limit (V): ") or -10
+        self.consts['Aux Output Upper Limit'] = \
+            input("Please enter Aux Output Upper Limit (V): ") or 0
+
+        self.consts['Signal Output Add'] = \
+            input("Please enter Signal Output Add (0: False, 1: True): ") or 1
+
+        self.consts['Aux Output Signal'] = \
+            input("Please enter Aux Output Signal (0: Off, 1: Osc Phi Demod 2, \
+                  36: Threshold 1, 37: Threshold 2, 38: Threshold 3, \
+                  39: Threshold 4, 52: MDS Sync Out): ") or 36
+                
+    def setConstants(self):
+        self.session.daq_server.set('/dev32271/imps/0/freq', 
+                                    self.consts['Oscillation Frequency'])
+        self.session.daq_server.set('/dev32271/imps/0/maxbandwidth', 
+                                    self.consts['Max bandwidth'])
+        self.session.daq_server.set('/dev32271/imps/0/auto/inputrange', 
+                                    self.consts['Input Control'])
+        self.session.daq_server.set('/dev32271/imps/0/current/range', 
+                                    self.consts['Current Range'])
+        self.session.daq_server.set('/dev32271/imps/0/voltage/range', 
+                                    self.consts['Voltage Range'])
+        self.session.daq_server.set('/dev32271/imps/0/omegasuppression', 
+                                    self.consts['Omega Suppression'])
+        self.session.daq_server.set('/dev32271/imps/0/demod/rate', 
+                                    self.consts['Data Transfer Rate'])
+        self.session.daq_server.set('/dev32271/imps/0/mode', 
+                                    self.consts['Equivalent Circuit Mode'])
+        
+        self.session.daq_server.set('/dev32271/tu/thresholds/0/input', 
+                                    self.consts['Threshold Input Signal'])
+        self.session.daq_server.set('/dev32271/tu/thresholds/0/activationtime', 
+                                    self.consts['State Enable Time'])
+        self.session.daq_server.set('/dev32271/tu/thresholds/0/deactivationtime', 
+                                    self.consts['State Disable Time'])
+        self.session.daq_server.set('/dev32271/tu/logicunits/0/inputs/0/not', 
+                                    self.consts['Logic Unit Not'])
+        
+        self.session.daq_server.set('/dev32271/auxouts/0/outputselect', 
+                                    self.consts['Aux Output Signal'])
+        self.session.daq_server.set('/dev32271/auxouts/0/scale', 
+                                    self.consts['Aux Output Scale'])
+        self.session.daq_server.set('/dev32271/auxouts/0/offset', 
+                                    self.consts['Aux Output Offset'])
+        self.session.daq_server.set('/dev32271/auxouts/0/limitlower', 
+                                    self.consts['Aux Output Lower Limit'])
+        self.session.daq_server.set('/dev32271/auxouts/0/limitupper', 
+                                    self.consts['Aux Output Upper Limit'])
+        
+        self.session.daq_server.set('/dev32271/sigouts/0/add', 
+                                    self.consts['Signal Output Add'])
+
+        self.session.daq_server.set('/dev32271/triggers/out/0/source', 
+                                    self.consts['Aux Output Signal'])
 
 
 
